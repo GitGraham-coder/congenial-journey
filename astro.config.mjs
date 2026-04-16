b4eb583b7d09a495e89484faa754d481120263d8
Graham <graham.miller403@gmail.com>
2026-04-16 12:58:27 +0100

    fixed rss

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
-// @ts-check//
+// @ts-check
 import { defineConfig } from 'astro/config';
 
-//https://astro.build/config//
-export default defineConfig({});
-site: "https://animated-pegasus-220049.netlify.app"  
+import preact from "@astrojs/preact";
+// https://astro.build/config
+export default defineConfig({
+  site: "https://animated-pegasus-220049.netlify.app",
+  integrations: [preact()],
+});
