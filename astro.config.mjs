0a7586145eee9db90b0fd1dc2cd38ed755b98957
Graham <graham.miller403@gmail.com>
2026-04-16 13:19:27 +0100

    Added Preact to Astro Project

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,11 @@
 import { defineConfig } from 'astro/config';
 
 
+import preact from "@astrojs/preact";
+
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://animated-pegasus-220049.netlify.app",
-  
+  integrations: [preact()],
 });
